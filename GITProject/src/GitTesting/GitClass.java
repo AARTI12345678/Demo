569421f9e5da2c72ebcdf259f454f4c6c29a07b3
Aarti <aartibisen1011@gmail.com>
2022-10-09 21:05:40 +0530

    First

---
+++ GITProject/src/GitTesting/GitClass.java
@@ -0,0 +1,24 @@
+package GitTesting;
+
+public class GitClass {
+
+			
+				public static void main(String[] args) {
+					// TODO Auto-generated method stub
+			//System.out.println("Hello Java");
+			System.setProperty("webdriver.chrome.driver","C:\\Users\\Dell\\Downloads\\chromedriver.exe");
+			//C:\Users\Dell\Downloads
+			//System.setProperty("webdriver.chrome.driver","C:\\Users\\Dell\\Downloads\\chromedriver_win32\\chromedriver.exe");
+					//WebDriver driver=new ChromeDriver();
+					//driver.get("https://chromedriver.chromium.org/downloads");
+					//driver.close();
+			System.out.println("Success");
+			
+			System.out.println("Success");
+				}
+
+			}
+
+
+
+}
